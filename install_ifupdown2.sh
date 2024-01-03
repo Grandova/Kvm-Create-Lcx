c1b73bd450d8095ad05caf91d8cc0cfb4b03114d
Yoshino <2593905944z@gmail.com>
2024-01-03 17:09:53 +0800

    Create install_ifupdown2.sh

---
+++ install_ifupdown2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 安装ifupdown2
+apt-get install -y ifupdown2
+echo "1" >"/usr/local/bin/ifupdown2_installed.txt"
+
+# 删除Systemd服务
+systemctl disable ifupdown2-install.service
+rm /etc/systemd/system/ifupdown2-install.service
+
+# 删除自身
+rm $0
